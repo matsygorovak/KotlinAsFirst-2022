34c94e39d4c201723bd16d10883eb7ad232e7107
matsygorovak <112812086+matsygorovak@users.noreply.github.com>
2022-09-13 17:43:21 +0300

    task solved (angleInRadian)

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -84,7 +84,9 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
+    return (deg * 3600 + min * 60 + sec) * PI / 648000
+}
 
 /**
  * Тривиальная (1 балл)
